0611a21be5b86ea17e29e5a6933018ae5bac4dbd
Evgeniy <zadrod93@yandex.ru>
2022-02-20 21:38:35 +0300

    Задача номер 34

---
+++ Example034/Program.cs
@@ -0,0 +1,27 @@
+﻿//34 Написать программу для замены элементов массива на противоположные.
+
+int[] array = new int[10];
+Random randNum = new Random();
+void FillArray(int[] array)
+{
+    int position  = 0;
+    while (position < array.Length)
+    {
+        array[position] = randNum.Next(-21,21);
+        Console.Write($"{array[position]}  ");
+        position ++;
+    }
+}
+void ContraryNum(int[] array)
+{
+    for(int index = 0; index < array.Length; index ++)
+    {
+        array[index] = array[index] * -1;
+        Console.Write($"{array[index]}  ");
+    }
+}
+Console.WriteLine("Дан массив : ");
+FillArray( array );
+Console.WriteLine();
+Console.WriteLine("Массив с противоположными знаками : ");
+ContraryNum( array );
